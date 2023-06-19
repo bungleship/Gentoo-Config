ab26e6b5e997a0251c27ff320587d52c8bbaae8b
bungleship <131033590+bungleship@users.noreply.github.com>
2023-06-19 12:12:00 -0700

    Update xmonad.hs

--- home/leo/xmonad/xmonad.hs
+++ home/leo/xmonad/xmonad.hs
@@ -52,7 +52,7 @@ myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
 -- Border colors for unfocused and focused windows, respectively.
 --
 myNormalBorderColor  = "#dddddd"
-myFocusedBorderColor = "#ff0000"
+myFocusedBorderColor = "#61538d"
 
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
